1e6a769d296c976fe4d78f6ee53a5b4fc32dbda6
Sara Felix <sfelix@MacBook-Air-de-Sara.local>
2023-05-12 18:53:41 -0300

    criando model, tableview e célula

---
+++ MarvelCList/MarvelCList/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MarvelCList
+//
+//  Created by Sara Felix on 12/05/23.
+//
+
+import Foundation
---
+++ MarvelCList/MarvelCList/Screens/CharacterListCell.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterListCell.swift
+//  MarvelCList
+//
+//  Created by Sara Felix on 12/05/23.
+//
+
+import Foundation
